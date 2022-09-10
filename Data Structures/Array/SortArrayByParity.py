79dd8729e052ca34eca4546a6aa24b78f787dde3
rusuraluca <rr.ralucarusu@gmail.com>
2022-09-10 20:23:16 +0300

    updates

---
+++ Data Structures/Array/SortArrayByParity.py
@@ -0,0 +1,21 @@
+"""
+Problem:
+-----------------------------------------------
+https://leetcode.com/problems/sort-array-by-parity/
+"""
+
+
+class Solution:
+    def sortArrayByParity(self, nums):
+        # transverse nums
+        # if nums[i] is even
+        # put it in first index(l) and increase l+=1
+        # if nums[i] is not even keep increasing second pointer
+        l = 0
+        r = 0
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[l], nums[r] = nums[i], nums[l]
+                l += 1
+            r += 1
+        return nums
